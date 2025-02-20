178be9e63502ecb0509405e6cbf37058370ae15d
oleg <olegmms@gmail.com>
2025-02-20 23:26:38 +0300

    fix(): add dotenv invocation

--- src/main/java/org/martinmeer/otkassistant/MainApp.java
+++ src/main/java/org/martinmeer/otkassistant/MainApp.java
@@ -1,5 +1,6 @@
 package org.martinmeer.otkassistant;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -8,12 +9,15 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class MainApp extends SpringBootServletInitializer {
 
+
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(MainApp.class);
     }
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
         SpringApplication.run(MainApp.class, args);
     }
 
