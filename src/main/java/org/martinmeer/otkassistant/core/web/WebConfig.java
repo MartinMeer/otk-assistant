fae8f236891461ffc9c0d842f87ec51bcd8445d4
oleg <olegmms@gmail.com>
2025-08-21 20:56:58 +0300

    add db sources

--- src/main/java/org/martinmeer/otkassistant/core/web/WebConfig.java
+++ src/main/java/org/martinmeer/otkassistant/core/web/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                //allowedOrigins("http://localhost:40155")
+                .allowedOrigins("http://localhost:3001")
                 .allowedOrigins("https://otk-help.martinmeer.com")
                 .allowedMethods("POST", "GET", "OPTIONS")
                 .allowedHeaders("*")
