364b362995071b9be648a290a5aa7443a4918cf9
oleg <olegmms@gmail.com>
2025-09-28 23:50:43 +0300

    mainApp:feat(docker): begin to refactor hardcoded things

---
+++ env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+export DATASOURCE_URL="" \
+&& export DATASOURCE_USERNAME="otkreader" \
+&& export DATASOURCE_PASSWORD="victoria" \
+&& export SERVER_PORT="" \
+&& export SPRING_PROFILES_ACTIVE="" \
+&& export ALLOWED_ORIGINS="" \
+&& export LOG_LEVEL_ROOT="" \
+&& export LOG_LEVEL_APP=""
